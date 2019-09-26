ddbecfcd312e3c297581d2d0635d95d9aba4195c
Monica Kann <monicak27@mail.fresnostate.edu>
2019-09-25 21:40:19 -0700

    update wireframe

--- src/index.js
+++ src/index.js
@@ -1,25 +1,33 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
-import { createStore, applyMiddleware, compose } from 'redux'
-import rootReducer from './store/reducers/rootReducer'
-import { Provider } from 'react-redux'
-import thunk from 'redux-thunk'; // middleware, halts dispatch
-import { reduxFirestore, getFirestore } from 'redux-firestore';
-import { reactReduxFirebase, getFirebase } from 'react-redux-firebase';
-import config from './config/fbConfig'
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import registerServiceWorker from "./registerServiceWorker";
+import { createStore, applyMiddleware, compose } from "redux";
+import rootReducer from "./store/reducers/rootReducer";
+import { Provider } from "react-redux";
+import thunk from "redux-thunk";
 
-const store = createStore(rootReducer,
+// react-redux-firebase v.3 update
+import { ReactReduxFirebaseProvider, getFirebase } from "react-redux-firebase";
+import { createFirestoreInstance } from "redux-firestore";
+import firebase from "./config/fbConfig";
+
+const store = createStore(
+  rootReducer,
   compose(
-    applyMiddleware(thunk.withExtraArgument({getFirebase, getFirestore})),
-    reactReduxFirebase(config, {userProfile: 'users', useFirestoreForProfile: true, attachAuthIsReady: true}),
-    reduxFirestore(config) // redux bindings for firestore
+    applyMiddleware(thunk.withExtraArgument({ getFirebase, getFirestore })),
+    redux()
   )
 );
 
+
 store.firebaseAuthIsReady.then(() => {
-  ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
+  ReactDOM.render(
+    <Provider store={store}>
+      <App />
+    </Provider>,
+    document.getElementById("root")
+  );
   registerServiceWorker();
 });
