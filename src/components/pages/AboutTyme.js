132c08d71f545e8b67b47fb0d159b25023de7923
Monica Kann <monicak27@mail.fresnostate.edu>
2019-10-15 19:48:51 -0700

    v0.1

---
+++ src/components/pages/AboutTyme.js
@@ -0,0 +1,112 @@
+import React, { useState } from "react";
+import { Redirect } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { signIn } from "../../store/actions/authActions";
+import { signInWithGoogle } from "../../store/actions/authActions";
+import { NavLink } from "react-router-dom";
+
+// Material Ui Components
+import { makeStyles } from "@material-ui/core/styles";
+import Avatar from "@material-ui/core/Avatar";
+import Button from "@material-ui/core/Button";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import TextField from "@material-ui/core/TextField";
+import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
+import Typography from "@material-ui/core/Typography";
+import Container from "@material-ui/core/Container";
+import Grid from "@material-ui/core/Grid";
+import { loginPageStyle } from "../../styles/components/auth/loginPageStyle";
+import GoogleButton from "react-google-button";
+
+const LogIn = () => {
+  const classes = loginPageStyle();
+  const [email, setEmail] = useState("");
+  const [pword, setPassword] = useState("");
+  const dispatch = useDispatch();
+  const auth = useSelector(state => state.firebase.auth);
+  const authError = useSelector(state => state.auth.authError);
+
+  const handleEmailChange = e => {
+    setEmail(e.target.value);
+  };
+
+  const handlePasswordChange = e => {
+    setPassword(e.target.value);
+  };
+  const handleSubmit = e => {
+    e.preventDefault();
+    dispatch(signIn(email, pword));
+  };
+
+  const googleLogIn = e => {
+    dispatch(signInWithGoogle());
+  };
+
+  if (auth.uid) return <Redirect to='/' />;
+  return (
+    <Container component='main' maxWidth='xs'>
+      <CssBaseline />
+      <div className={classes.paper}>
+        <Avatar className={classes.avatar}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component='h1' variant='h5'>
+          hey In
+        </Typography>
+        <form className={classes.form} onSubmit={handleSubmit} noValidate>
+          <TextField
+            variant='outlined'
+            margin='normal'
+            required
+            fullWidth
+            value={email}
+            id='email'
+            label='Email Address'
+            name='email'
+            autoComplete='email'
+            autoFocus
+            onChange={handleEmailChange}
+          />
+          <TextField
+            variant='outlined'
+            margin='normal'
+            required
+            fullWidth
+            value={pword}
+            name='password'
+            label='Password'
+            type='password'
+            id='password'
+            autoComplete='current-password'
+            onChange={handlePasswordChange}
+          />
+          <Typography component='h1' variant='h5'>
+            {authError}
+          </Typography>
+          <Button
+            type='submit'
+            fullWidth
+            variant='contained'
+            color='primary'
+            className={classes.submit}
+          >
+            About
+          </Button>
+          <GoogleButton
+            className={classes.googleButton}
+            onClick={googleLogIn}
+          />
+          <Grid container justify='center' className={classes.toSignUp}>
+            <Grid item>
+              <NavLink to='/signup'>
+                Don't have an account yet? Sign Up →
+              </NavLink>
+            </Grid>
+          </Grid>
+        </form>
+      </div>
+    </Container>
+  );
+};
+
+export default LogIn;
---
+++ src/styles/components/auth/signupPageStyle.js
@@ -0,0 +1,32 @@
+import { makeStyles, useTheme } from "@material-ui/core/styles";
+
+export const signupPageStyle = makeStyles(theme => ({
+    '@global': {
+      body: {
+        backgroundColor: theme.palette.common.white,
+      },
+    },
+    paper: {
+        marginTop: "50%",
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+    },
+    avatar: {
+      margin: theme.spacing(1),
+      backgroundColor: "#FFC107"
+    },
+    form: {
+      width: '100%', // Fix IE 11 issue.
+      marginTop: theme.spacing(3),
+    },
+    submit: {
+        margin: theme.spacing(3, 0, 2),
+        backgroundColor: "black",
+        color: "white",
+        "&:hover": {
+          backgroundColor: "#FFC107"
+        }
+      }
+  }));
+  
