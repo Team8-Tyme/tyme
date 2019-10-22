72cc5189f9d106e84102a044451361d995f410c9
Monica Kann <monicak27@mail.fresnostate.edu>
2019-10-22 16:06:36 -0700

    login edits

--- src/store/actions/authActions.js
+++ src/store/actions/authActions.js
@@ -15,6 +15,38 @@ export const signIn = (email, password) => {
     }
   }
   
+  export const signInWithGoogle = () => {
+    return (dispatch, getState, { getFirebase }) => {
+      const firebase = getFirebase();
+      var provider = new firebase.auth.GoogleAuthProvider();
+      //console.log(provider);
+      firebase
+        .auth()
+        .signInWithPopup(provider)
+        .then(function(result) {
+          // This gives you a Google Access Token. You can use it to access the Google API.
+          var token = result.credential.accessToken;
+          // The signed-in user info.
+          var user = result.user;
+
+          console.log(token);
+          console.log(user);
+          // ...
+        })
+        .catch(function(error) {
+          // Handle Errors here
+          var errorCode = error.code;
+          var errorMessage = error.message;
+          // The email of the user's account used.
+          var email = error.email;
+          // The firebase.auth.AuthCredential type that was used.
+          var credential = error.credential;
+          // ...
+        });
+    };
+  };
+  
+
   export const signOut = () => {
     return (dispatch, getState, {getFirebase}) => {
       const firebase = getFirebase();
