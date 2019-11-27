a5c1ed673ddcc88c9810a37d9394cb1650ba001b
Calender147 <jcalender147@gmail.com>
2019-11-27 15:54:39 -0800

    basic addition of history feature

--- src/config/routes.js
+++ src/config/routes.js
@@ -2,11 +2,13 @@
 import Dashboard from "../components/dashboard/Dashboard";
 import CreateProject from "../components/pages/createProject"
 import Settings from "../components/pages/Settings"
+import History from "../components/pages/History"
 
 // @material-ui/icons
 import DashboardIcon from "@material-ui/icons/Dashboard";
 import WorkIcon from "@material-ui/icons/Work";
 import SettingsIcon from "@material-ui/icons/Settings"
+import HistoryIcon from "@material-ui/icons/History"
 
 
 const mainRoutes = [
@@ -30,6 +32,13 @@ const mainRoutes = [
     mini: "Setting",
     icon: SettingsIcon,
     component: Settings
+  },
+  {
+    path: "/history",
+    name: "History",
+    mini: "Hist",
+    icon: HistoryIcon,
+    component: History
   },
 //   {
 //     path: "/tables/members",
