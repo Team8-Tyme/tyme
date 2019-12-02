fb4a2897fed65e75acc3110f0ff13d6f1891913a
Monica Kann <monicak27@mail.fresnostate.edu>
2019-12-02 13:43:10 -0800

    minor styling changes

--- src/components/habits/HabitStreaks.js
+++ src/components/habits/HabitStreaks.js
@@ -2,51 +2,41 @@ import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import moment from "moment";
 import { Paper, Typography } from "@material-ui/core";
-import { makeStyles } from "@material-ui/core/styles";
-
+import Button from "@material-ui/core/Button";
 export default class HabitStreaks extends Component {
   constructor(props) {
     super(props);
   }
-  
-  render() {
-  
-     let habits = JSON.parse(window.localStorage.getItem("habits"));
-     let habit = habits.filter(
-       item => item.id === parseInt(this.props.match.params.id)
-     )[0];
 
-    
-    let daysRecorded = habit.days;
+  render() {
+    let habits = JSON.parse(window.localStorage.getItem("habits"));
+    let habit = habits.filter(
+      item => item.id === parseInt(this.props.match.params.id)
+    )[0];
+
+    let habitDays = habit.days;
     let dayGrids = [];
     for (let i = 0; i < 365; i++) {
       let dayGridDate = moment()
         .subtract(i, "days")
         .format("YYYY-MM-D");
-      if (dayGridDate.toString() === daysRecorded[daysRecorded.length - 1]) {
+      if (dayGridDate.toString() === habitDays[habitDays.length - 1]) {
         dayGrids.unshift(<div className="day day--active" key={i} />);
-        daysRecorded.splice(daysRecorded.length - 1, 1);
+        habitDays.splice(habitDays.length - 1, 1);
       } else {
         dayGrids.unshift(<div className="day" key={i} />);
       }
     }
 
     return (
-      <Paper>
-        <div className="history">
-          <div className="history__header">
-            <div>
-              <Typography>{habit.name}</Typography>
-              <div className="history__duration">Last 365 Days</div>
-            </div>
-            <span className="history__close">
-              <Link to="/dashboard" className="button button--danger">
-                Back
-              </Link>
-            </span>
-          </div>
-          <div className="days">{dayGrids}</div>
-        </div>
+      <Paper className="habitGraph">
+        <Typography>{habit.name}</Typography>
+        <Typography>Last 365 Days</Typography>
+        <div className="days">{dayGrids}</div>
+
+        <Button>
+          <Link to="/dashboard">Close</Link>
+        </Button>
       </Paper>
     );
   }
